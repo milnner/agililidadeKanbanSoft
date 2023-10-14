809e1661afe07ecb6789ef94adcdd7009a4c5bc1
pedzero <pedro9669silva@gmail.com>
2023-10-14 18:17:59 -0300

    Implementação da classe KanbanPerms

---
+++ src/main/java/ifsuldeminas/pas/bcc/KanbanSoftware/model/KanbanPerms.java
@@ -0,0 +1,11 @@
+package ifsuldeminas.pas.bcc.KanbanSoftware.model;
+
+// revision needed
+public enum KanbanPerms {
+    VIEW,
+    CREATE_TASK,
+    UPDATE_TASK,
+    DELETE_TASK,
+    MOVE_TASK,
+    ASSIGN_TASK
+}
