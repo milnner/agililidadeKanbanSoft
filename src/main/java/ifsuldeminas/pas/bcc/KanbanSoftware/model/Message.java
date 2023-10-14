72bb147c17e375b086d3745efd65ade37db78031
pedzero <pedro9669silva@gmail.com>
2023-10-14 18:18:45 -0300

    Implementação da classe Message

---
+++ src/main/java/ifsuldeminas/pas/bcc/KanbanSoftware/model/Message.java
@@ -0,0 +1,9 @@
+package ifsuldeminas.pas.bcc.KanbanSoftware.model;
+
+import java.time.LocalDateTime;
+
+public class Message {
+    private int id;
+    private LocalDateTime creationDate;
+    private String messageContent;
+}
