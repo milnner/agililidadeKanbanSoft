98f6ad21abb1642ca92519fd380db7c61f91cf9a
pedzero <pedro9669silva@gmail.com>
2023-10-14 17:45:14 -0300

    Primeiro commit

---
+++ src/main/java/ifsuldeminas/pas/bcc/KanbanSoftware/KanbanSoftwareApplication.java
@@ -0,0 +1,13 @@
+package ifsuldeminas.pas.bcc.KanbanSoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ifsuldeminas/pas/bcc/KanbanSoftware/KanbanSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package ifsuldeminas.pas.bcc.KanbanSoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
