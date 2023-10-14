6a595097f499a5023defd6837e1dfd71e88af93b
pedzero <pedro9669silva@gmail.com>
2023-10-14 18:18:28 -0300

    Implementação da classe UserChatConnection

---
+++ src/main/java/ifsuldeminas/pas/bcc/KanbanSoftware/model/UserChatConnection.java
@@ -0,0 +1,8 @@
+package ifsuldeminas.pas.bcc.KanbanSoftware.model;
+
+import java.time.LocalDateTime;
+
+public class UserChatConnection {
+    private int id;
+    private LocalDateTime creationDate;
+}
