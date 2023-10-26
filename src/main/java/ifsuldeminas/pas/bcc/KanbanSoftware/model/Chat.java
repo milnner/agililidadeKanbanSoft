dfbf658d6d91e0acb9b7bd9f4e21b2c34de4db71
milnner <milnnernj@gmail.com>
2023-10-25 22:56:11 -0300

    Relacionamento adicionado dado a cardinalidade

--- src/main/java/ifsuldeminas/pas/bcc/KanbanSoftware/model/Chat.java
+++ src/main/java/ifsuldeminas/pas/bcc/KanbanSoftware/model/Chat.java
@@ -1,8 +1,11 @@
 package ifsuldeminas.pas.bcc.KanbanSoftware.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class Chat {
     private int id;
     private LocalDateTime creationDate;
+    private ArrayList<Integer> userChatConnectionIds;
+
 }
