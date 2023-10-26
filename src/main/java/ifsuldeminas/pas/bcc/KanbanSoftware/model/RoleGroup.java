c798c46058ce16172824a61be99dc6149923f439
milnner <milnnernj@gmail.com>
2023-10-25 22:56:00 -0300

    Relacionamento adicionado dado a cardinalidade

--- src/main/java/ifsuldeminas/pas/bcc/KanbanSoftware/model/RoleGroup.java
+++ src/main/java/ifsuldeminas/pas/bcc/KanbanSoftware/model/RoleGroup.java
@@ -1,6 +1,13 @@
 package ifsuldeminas.pas.bcc.KanbanSoftware.model;
 
+import java.util.ArrayList;
+
 public class RoleGroup {
     private int id;
     private String name;
+    private ArrayList<Integer> boardIds;
+    private ArrayList<Integer> cardIds;
+
+    private ArrayList<Integer> listIds;
+    private ArrayList<Integer> roleIds;
 }
