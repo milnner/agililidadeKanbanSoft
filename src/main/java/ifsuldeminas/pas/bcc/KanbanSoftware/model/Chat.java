7c813d68b64823e4709aee8ab3a30d003606a9fa
pedzero <pedro9669silva@gmail.com>
2023-11-16 16:31:50 -0300

    Mapeamento relacional de entidade

--- src/main/java/ifsuldeminas/pas/bcc/KanbanSoftware/model/Chat.java
+++ src/main/java/ifsuldeminas/pas/bcc/KanbanSoftware/model/Chat.java
@@ -1,11 +1,22 @@
 package ifsuldeminas.pas.bcc.KanbanSoftware.model;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
+@Table(name = "chats")
+@Entity(name = "chats")
 public class Chat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private int id;
+    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime creationDate;
+    @ManyToOne
+    @JoinColumn(name = "chat_connection_ids")
     private ArrayList<Integer> userChatConnectionIds;
-
+    @ManyToOne
+    @JoinColumn(name = "message_ids")
+    private Message message;
 }
