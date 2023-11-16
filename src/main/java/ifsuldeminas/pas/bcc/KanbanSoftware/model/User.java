0d159d23488c867ff0c83f57eeb333c13e1bc4c5
pedzero <pedro9669silva@gmail.com>
2023-11-16 16:31:56 -0300

    Mapeamento relacional de entidade

--- src/main/java/ifsuldeminas/pas/bcc/KanbanSoftware/model/User.java
+++ src/main/java/ifsuldeminas/pas/bcc/KanbanSoftware/model/User.java
@@ -1,18 +1,26 @@
 package ifsuldeminas.pas.bcc.KanbanSoftware.model;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
+@Table(name = "users")
+@Entity(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private int id;
+    @Column(unique = true)
     private String nickname;
     private String name;
     private String surname;
     private LocalDateTime entryDate;
     private LocalDateTime bournDate;
+    @Column(unique = true)
     private String email;
     private String passwordHash;
+    @Enumerated(EnumType.STRING)
     private Sex sex;
     private ArrayList<Integer> rolegroupIds;
-
 }
